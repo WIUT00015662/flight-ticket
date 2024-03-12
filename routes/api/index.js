b94efe3a9fca1c145654b1ee9e9f15ff8c90db0d
N1Dovud <skaiqhcvab72@bk.ru>
2024-03-12 02:39:27 -0400

    Problematic commit

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,23 +1,7 @@
-const express = require('express');
-const { validationResult } = require('express-validator');
-const { addTicketValidation } = require('../../../validators/ticket');
+const express = require('express')
+const ticket_router = require('./ticket')
 
-const router = express.Router();
-const ticket_controller = require('../../../controllers/api/ticket');
+const router = express.Router()
 
-// Define API routes
-router.get('/', (req, res)=>{
-    ticket_controller.getAll(req, res);
-});
-
-router.post('/', addTicketValidation(), (req, res)=>{
-    
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    ticket_controller.create(req, res)
-})
-
-module.exports = router;
+// registering child routers
+router.use('/ticket', ticket_router)
--- routes/api/ticket/index.js
+++ routes/api/ticket/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { validationResult } = require('express-validator');
+const { addTicketValidation } = require('../../../validators/ticket');
 
 const router = express.Router();
 const ticket_controller = require('../../../controllers/api/ticket');
@@ -8,5 +10,13 @@ router.get('/', (req, res)=>{
     ticket_controller.getAll(req, res);
 });
 
-module.exports = router;
+router.post('/', addTicketValidation(), (req, res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
+    ticket_controller.create(req, res)
+})
+
+module.exports = router;
