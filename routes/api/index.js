4c5ebba9ebc0b4729a78b99b350ab746d95215c6
N1Dovud <skaiqhcvab72@bk.ru>
2024-03-12 03:22:42 -0400

    final commits

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,41 +1,8 @@
-const express = require('express');
-const { validationResult } = require('express-validator');
-const { addTicketValidation, updateTicketValidation, deleteTicketValidation } = require('../../../validators/ticket');
+const express = require('express')
+const ticket_router = require('./ticket')
 
-const router = express.Router();
-const ticket_controller = require('../../../controllers/api/ticket');
+const router = express.Router()
 
-// Define API routes
-router.get('/', (req, res)=>{
-    ticket_controller.getAll(req, res);
-});
-
-router.post('/', addTicketValidation(), (req, res)=>{
-    
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    ticket_controller.create(req, res)
-})
-
-router.put('/:id', updateTicketValidation(), (req, res)=>{
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  ticket_controller.update(req, res)
-})
-
-router.delete('/:id', deleteTicketValidation(), (req, res, next)=>{
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  ticket_controller.delete(req, res)
-})
-
-module.exports = router;
+// registering child routers
+router.use('/ticket', ticket_router)
+module.exports = router
